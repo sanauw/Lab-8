82cf405c52215411ef3228e3d916736d6939d5b1
Luffy <281132937@formanite.fccollege.edu.pk>
2025-04-25 10:53:43 +0500

    Update user_experience.py

--- lab07-git-main/user_experience.py
+++ lab07-git-main/user_experience.py
@@ -140,7 +140,11 @@ def skip_question(allowed_skips):
     This function checks if the player has any skips available. If so, it decrements the allowed_skips counter and returns True, indicating the question can be skipped. If no skips are available, it returns False. This function should be called before presenting a new question to the player.
     """
     #------------------------
-    # Add your code here
+    if allowed_skips>1:
+        allowed_skips = allowed_skips - 1
+        return True
+    else:
+        return False 
     #------------------------
     raise NotImplementedError("This function is not implemented yet.")
     #------------------------
